b29708ea88f543352439c4ba86a55cf6831fc1ee
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 12:55:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('GATABOT-MD', {
+CFonts.say('GATABOT\nMD', {
   colors: ['blueBright','yellowBright'],                                        font: 'block',
   align: 'center',
 })
