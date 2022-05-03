f668e775e74c694a57936c5f1da8e1db5f51775f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 14:52:33 -0500

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,7 +1,7 @@
-/*let { execSync } = require('child_process')
+let { execSync } = require('child_process')
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url https://github.com/GataNina-Li/GataBot-MD && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url https://github.com/GataNina-Li/GataBot-MD.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
@@ -22,28 +22,4 @@ handler.fail = null
 handler.exp = 0
 
 module.exports = handler
-*/
-let { execSync } = require('child_process')
-let handler = async (m, { conn, text }) => {
-  if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-    conn.reply(m.chat, stdout.toString(), m)
-  }
-}
-handler.help = ['update']
-handler.tags = ['host']
-handler.command = /^update|actualizar$/i 
-handler.rowner = false
-handler.mods = false
-handler.premium = true
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-
-module.exports = handler
