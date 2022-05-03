8543c8143ba689420bcf18894f77d7b02a603a16
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 15:41:25 -0500

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,17 +1,17 @@
 let { execSync } = require('child_process')
-let handler = async (m, { conn, text, isROwner }) => {
+let handler = async (m, { conn, text }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url https://github.com/GataNina-Li/GataBot-MD.git && git pull' + (isROwner && text ? ' ' + text : ''))
-    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
-    m.reply(stdout.toString())
+    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
+    conn.reply(m.chat, stdout.toString(), m)
   }
 }
 handler.help = ['update']
-handler.tags = ['owner']
-handler.command = /^update$/i
-handler.owner = true
+handler.tags = ['host']
+handler.command = /^update|actualizar$/i 
+handler.rowner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = false
 handler.private = false
 
@@ -22,4 +22,3 @@ handler.fail = null
 handler.exp = 0
 
 module.exports = handler
-
