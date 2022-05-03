3fe9897780f9c0587714f1e9275b0d090558844f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 12:53:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('GATABOT-MD\nPOR\nGATA DIOS', {
+CFonts.say('GATABOT-MD', {
   colors: ['blueBright','yellowBright'],                                        font: 'block',
   align: 'center',
 })
-CFonts.say(`BY HYZER OFFICIAL`, {                                            
+CFonts.say(`POR GATA DIOS | MULTI-DISPOSITIVOS`, {                                            
 colors: ['yellow'],                                                           
 font: 'console',                                                              align: 'center',
 })
