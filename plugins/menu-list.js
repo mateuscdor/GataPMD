7cb8fc181983190ab036249893401d23d5285f17
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-04 10:41:56 -0500

    Update menu-list.js

--- plugins/menu-list.js
+++ plugins/menu-list.js
@@ -256,11 +256,11 @@ let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(
               {
                 "rows": [{
                   "title": `𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤 | 𝙊𝙬𝙣𝙚𝙧`,
-                  "description": "*GataBot-MD no responde a dudas. Solo Gata Dios*\n*GataBot-MD does not answer questions. Only Gata Dios.*",
+                  "description": "GataBot-MD no responde a dudas. Solo Gata Dios\nGataBot-MD does not answer questions. Only Gata Dios.",
                   "rowId": `.owner`
                 },{
-                  "title": "INFO BOT",
-                  "description": "Menampilkan Menu Info",
+                  "title": "𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞𝙤́𝙣 𝙙𝙚 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣",
+                  "description": "Conoce más del menú\nLearn more about the menu",
                   "rowId": `${_p}? info`
                 }],
                 "title": "INFORMASI BOT"
