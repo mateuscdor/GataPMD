388ec214f0c31f64c172b62675ac33c2af533530
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-04 01:22:15 -0500

    Update menu-list.js

--- plugins/menu-list.js
+++ plugins/menu-list.js
@@ -490,7 +490,7 @@ function clockString(ms) {
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 function ucapan() {
-  const time = moment.tz('America/Chicago').format('HH')
+  const time = moment.tz('America/Los_Angeles').format('HH')
   res = "Selamat DiniHari"
   if (time >= 6) {
     res = "🌇 *Buenos Días | Good Morning* ⛅"
