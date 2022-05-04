17073d31c85fd32430c93a445ec1d71834362a92
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 20:38:51 -0500

    Update menu-list.js

--- plugins/menu-list.js
+++ plugins/menu-list.js
@@ -229,22 +229,22 @@ let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(
             description: `╭━━━━━━〔 *${wm}* 〕━━━━━━⬣	    
 ┃✪ *Tiempo Actual | Current Time*	    
 ┃➺ ${time}   
-┃    ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃✪ *Activa durante | Active during* 
 ┃➺ ${uptime}
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃✪ *Versión de GataBot-MD*
 ┃➺ ${vs}
-┃    ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃✪ *Usuario(s) | Users*
 ┃➺ ${Object.keys(global.db.data.users).length} 
-┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃✪ *Modo | Mode*
 ┃➺ ${global.opts['self'] ? 'Self' : 'publik'}
-┃    ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃✪ *Chat(s) Prohibido(s) | Forbidden Chats*
 ┃➺ ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
-┃    ┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃✪ *Usuario(s) Prohibido(s) | Prohibited Users*
 ┃➺ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length}
 ╰━━━━━━━━━━━━━━━━━━━━━⬣`,
