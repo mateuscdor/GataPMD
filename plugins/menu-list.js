4e54f322ce0746fcf616f2573e2da8333913a80e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-04 15:58:02 -0500

    Update menu-list.js

--- plugins/menu-list.js
+++ plugins/menu-list.js
@@ -451,12 +451,15 @@ let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(
            hydratedButtons: [{
             urlButton: {
                displayText: 'MY WEBSITE',
-               url: 'https://Hyzerr.github.io/'
-		    
-		    conn.sendHydrated(m.chat, text.trim(), '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝', pp, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
-      ['𝙳𝙾𝙽𝙰𝚁', '/donasi'],
-      ['𝙾𝚆𝙽𝙴𝚁', '/owner']
-    ], m,)
+               url: 'https://Hyzerr.github.io/'	       
+		}
+
+           },
+             {
+              urlButton: {
+               displayText: 'MY WEBSITE',
+               url: 'https://Hyzerr.github.io/'	       
+                 
              }
 
            },
