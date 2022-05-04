fb610863252458cc43760cd172cf2c27d0aaad77
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 23:00:21 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -17,7 +17,7 @@ const q = {
 
 conn.sendContact(m.chat, '14509544207', 'Gata Dios', m) 
 conn.sendContact(m.chat, '593993684821', 'Oficial GataBot', m)
-m.reply("${gt} 🧡 Eso son los contactos para ti\nThat's the contacts for you")
+m.reply(`${gt}` "🧡 Eso son los contactos para ti\nThat's the contacts for you")
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
